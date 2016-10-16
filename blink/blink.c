09797d819a497072da3d7df5a0907f7e6b6c602c
cody <sebastian.kotulla@gmx.de>
2016-10-17 00:21:37 +0200

    First commit.

---
+++ blink/blink.c
@@ -0,0 +1,27 @@
+#include "stm32f1xx.h"
+#include <stdint.h>
+
+extern int32_t rust_main();
+int32_t i;
+
+int main(void)
+{
+    SystemInit();
+    SystemCoreClockUpdate();
+
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    GPIOC->CRH=0x33333333;
+
+	i = rust_main();
+	
+    while(1)
+    {
+        volatile int i = 0;
+
+        for(i=0;i<0x4000;i++){
+        }
+
+        GPIOC->ODR ^= (1 << 13);
+    }
+}
